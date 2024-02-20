4f69f6cdcfb2a2c2d06e6fddd640cf7800072f23
minnjung0887921 <minjeong0887921@gmail.com>
2024-02-20 13:14:38 +0900

    출력코드 수정

--- src/assignment/week1/HelloWorld/HelloWorld.java
+++ src/assignment/week1/HelloWorld/HelloWorld.java
@@ -1,8 +1,10 @@
 package assignment.week1.HelloWorld;
 
+import edu.princeton.cs.algs4.StdOut;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        StdOut.println("Hello, World");
     }
 }
